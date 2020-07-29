7b241cb7e9b62e0d7c1934e221d65f0fe3ac40f6
keyeh <keyeh@users.noreply.github.com>
2020-07-28 20:32:37 -0700

    cloud function

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import escape
+from vp_analyzer import HandAnalyzer, DiscardValue
+
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'cards' in request_json:
+        cards = request_json['cards']
+    elif request_args and 'cards' in request_args:
+        cards = request_args['cards']
+    else:
+        cards = 'World'
+    return HandAnalyzer(cards)
