81c1b843b170109a4a593eab484e0f3e12130823
Kevin Yeh <kyeh@apple.com>
2020-08-01 10:35:43 -0700

    all hands numbers

--- make_all_hands_file.py
+++ make_all_hands_file.py
@@ -2,6 +2,21 @@ import sys
 from all_hands_analysis import all_hands_gen, hand2str
 import os.path
 from os import path
+import json
+
+with open('deck.json') as f:
+  cardToNumber = json.load(f)
+
+def toNumber(cardStr):
+    return(cardToNumber[cardStr])
+
+
+def hand2str(hand_tup):
+    hstr = ''
+    for r, s in hand_tup:
+        n = toNumber(r + s)
+        hstr += str(n).zfill(2)
+    return hstr
 
 all_hands_str_l = list(map(hand2str, all_hands_gen()))
 
